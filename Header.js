e354c893d12ed7dba97346d1f3c691f21ffbde45
Kajsa Bootin <kajsa@Kajsas-Air.zyxel.com>
2020-05-04 16:40:03 +0200

    change

--- Header.js
+++ Header.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import styled from 'styled-components/native'
+import React from "react";
+import styled from "styled-components/native";
 
-export default function Header () {
-
-
+export default function Header() {
   return (
-    <View >
+    <View>
       <Text> MAGIC 8 BALL </Text>
     </View>
-  )
+  );
 }
 
 const View = styled.View`
   margin-top: 120;
   height: 100;
   width: 100%;
-  background-color: #FCED84;
+  background-color: #fced84;
   justify-content: center;
   align-items: center;
-`
+`;
 const Text = styled.Text`
   font-size: 50;
-`
+`;
--- MagicBall.js
+++ MagicBall.js
@@ -1,56 +1,54 @@
-import React, { useState} from "react"
-import styled from 'styled-components/native'
+import React, { useState } from "react";
+import styled from "styled-components/native";
 
 const quotes = [
-  'It is certain.',
-  'It is decidedly so.',
-  'Without a doubt.',
-  'You may rely on it.',
-  'As I see it, yes.',
-  'Most likely.',
-  'Outlook good.',
-  'Yes.',
-  'Signs point to yes.',
-  'Reply hazy, try again.',
-  'Ask again later.',
-  'Better not tell you now.',
-  'Cannot predict now.',
-  'Concentrate and ask again.',
-  'My reply is no.',
-  'My sources say no.', 
-  'Outlook not so good.',
-  'Very doubtful.',
-  ]  
-    
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful.",
+];
 
-export default function MagicBall  ()  {
-  const [quote, setQuote] = useState(0) 
+export default function MagicBall() {
+  const [quote, setQuote] = useState(0);
 
   const showQuote = () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length)
-    setQuote(quotes[randomIndex])
-  }
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  };
 
   return (
-
     <View>
       <BigCircel>
-          <TextNumber> 8</TextNumber>
-          <SmallCircel>
-            <Answear> {quote} </Answear>
-          </SmallCircel>
+        <TextNumber> 8</TextNumber>
+        <SmallCircel>
+          <Answear> {quote} </Answear>
+        </SmallCircel>
       </BigCircel>
       <TouchableOpacity onPress={showQuote}>
         <Text>Ask me</Text>
       </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 const View = styled.View`
   flex: 1;
-  height: 210; 
-`
+  height: 210;
+`;
 
 const BigCircel = styled.View`
   height: 320;
@@ -60,7 +58,7 @@ const BigCircel = styled.View`
   border-radius: 320;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const SmallCircel = styled.View`
   height: 190;
@@ -69,11 +67,11 @@ const SmallCircel = styled.View`
   border-radius: 100;
   justify-content: center;
   align-items: center;
-`
+`;
 const Text = styled.Text`
   font-size: 40;
   text-align: center;
-`
+`;
 
 const TextNumber = styled.Text`
   color: #fff;
@@ -81,15 +79,15 @@ const TextNumber = styled.Text`
   margin-top: -60;
   padding-bottom: 20;
   font-weight: bold;
-`
+`;
 const Answear = styled.Text`
   font-size: 25;
   text-align: center;
   font-weight: bold;
-`
+`;
 const TouchableOpacity = styled.TouchableOpacity`
-  background-color: #FCED84;
+  background-color: #fced84;
   border-radius: 50;
   justify-content: space-between;
   margin-top: 15;
-`
+`;
